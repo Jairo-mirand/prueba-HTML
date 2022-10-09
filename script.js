98fbc35b04c92ffe27792a1dc48a00429d765796
Jairo Miranda <114257578+Jairo-mirand@users.noreply.github.com>
2022-10-09 17:37:50 -0500

    3 commit

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-function letTest(){
-    let x=31;
-    if(true){
-        let x=71;
-        console.log(x);
-    }
-    console.log(x);
-
-}
+let frutas=["manzanas","peras","fresas"]
+let nuevalong=frutas.unshift("moras")
+let posicion=frutas.indexOf("fresas")
+console.log(posicion)
+let copia=frutas.slice();
+console.log(copia)
